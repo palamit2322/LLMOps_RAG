53098a3524a77e2f590cd0bebad950c283c92bc2
Amit Pal <amitpal@Amits-MacBook-Pro.local>
2026-01-10 22:38:52 +0530

    Optimised the logging code

--- multi_doc_chat/logger/custom_cloud_logging.py
+++ multi_doc_chat/logger/custom_cloud_logging.py
@@ -1,33 +1,31 @@
-from datetime import datetime
 import structlog
 import logging
 
 class Cloud_Custom_Logger():
     _configured=False
-    def get_cloud_custom_logger(self,name:str):
-
-        if not Cloud_Custom_Logger._configured:
+    @classmethod
+    def get_cloud_custom_logger(cls,name:str):
+        if not cls._configured:
+            handlers=[logging.StreamHandler()]
             logging.basicConfig(
                 level=logging.INFO,
-                handlers=[logging.StreamHandler()]
+                handlers=handlers
             )
-
-        structlog.configure(
-            processors=[
-                structlog.processors.TimeStamper(fmt="iso",utc=True),
-                structlog.processors.add_log_level,
-                structlog.processors.CallsiteParameterAdder(
-                    {
-                        structlog.processors.CallsiteParameter.FILENAME,
-                        structlog.processors.CallsiteParameter.LINENO
-                    }
-                ),
-                structlog.processors.JSONRenderer()
-            ],
-            logger_factory=structlog.stdlib.LoggerFactory(),
-            cache_logger_on_first_use=True
-        )  
-        Cloud_Custom_Logger._configured-True
-
+            structlog.configure(
+                processors=[
+                    structlog.processors.TimeStamper(fmt="iso",utc=True),
+                    structlog.processors.add_log_level,
+                    structlog.processors.CallsiteParameterAdder(
+                        {
+                            structlog.processors.CallsiteParameter.FILENAME,
+                            structlog.processors.CallsiteParameter.LINENO
+                        }
+                    ),
+                    structlog.processors.JSONRenderer()
+                ],
+                logger_factory=structlog.stdlib.LoggerFactory(),
+                cache_logger_on_first_use=True
+            )  
+            cls._configured=True
         return structlog.get_logger(name)  
 
